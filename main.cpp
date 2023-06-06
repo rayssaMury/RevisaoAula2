19f6195f1f017e737d5c01061b84b2d503e203a3
Rayssa <rayssa.mury@aluno.cefet-rj.br>
2023-06-06 14:31:25 -0300

    adição e subtração

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlinb.h>
+
+using namespace;
+
+int calc(int numOne, inte numTwo, int calcOperador, int &aux){
+    int result
+    switch(calcOperador){
+    case '+':
+    result = numOne + numTwo;
+    break;
+    case '-':
+    result = numOne - numTwo;
+}
+}
